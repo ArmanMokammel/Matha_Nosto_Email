316728dc83bd14ac398b3a18a88631551a9a7100
Arman Mokammel <armanmokammel@gmail.com>
2024-07-16 00:31:34 +0600

    .

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -5,26 +5,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from email_templater.models import Email_Template
 
-
 # Create your views here.
-
-# Create your views here.
-def login_page(request):
+def login(request):
   if request.method == 'POST':
-       if request.POST.get('login_button'):
+       if request.POST.get('Log-in'):
             username_ = request.POST.get('username')
             password_ = request.POST.get('password')
-
-            print(username_)
-            print(password_)
 
             user = auth.authenticate(username = username_, password=password_)
             if user is not None:
                  auth.login(request, user)
-                 return redirect('note_book_user:dashboard')
+                 return redirect('users:dashboard')
             else: 
                  messages.error(request, 'Invalid credentials/User not registered.')
-                 return redirect('note_book_user:login_page')
+                 return redirect('users:login')
                  
 
   return render(request, "login.html")
@@ -40,25 +34,26 @@ def dashboard(request):
 
 def signup(request):
   if request.method == "POST":
-     if request.POST.get('sign_up'):
-              #clicked sign up button
-              username_ = request.POST.get('username')
-              password_ = request.POST.get('password')
-              confirm_password_ = request.POST.get('confirm_password')
+     print(request.POST)
+     if request.POST.get('Sign-Up'):
+          #clicked sign up button
+          username_ = request.POST.get('username')
+          password_ = request.POST.get('password')
+          confirm_password_ = request.POST.get('confirm_password')
 
-              if password_ == confirm_password_:
-                      if User.objects.filter(username = username_).exists():
-                           messages.warning(request, 'User already exist')
-                           return redirect('note_book_user:signup')
-                        
-                      else:
-                          user = User.objects.create_user(username = username_, password = password_)
-                          user.save()
-                          messages.success(request, "User created successfully")
-                          return redirect('note_book_user:login_page')
-              else: 
-                  messages.error(request, "Password did not match")
-                  return redirect('note_book_user:signup')
+          if password_ == confirm_password_:
+               if User.objects.filter(username = username_).exists():
+                    messages.warning(request, 'User already exist')
+                    return redirect('users:signup')
+               
+               else:
+                    user = User.objects.create_user(username = username_, password = password_)
+                    user.save()
+                    messages.success(request, "User created successfully")
+                    return redirect('users:login')
+          else: 
+               messages.error(request, "Password did not match")
+               return redirect('users:signup')
             
        
   return render(request, "signup.html")
@@ -66,4 +61,4 @@ def signup(request):
 @login_required
 def logout(request):
      auth.logout(request)
-     return redirect('note_book_user:login_page')
+     return redirect('users:login')
