b86fd736c685154c76698a2961473ed0d9157a4d
developerhuzaifaahmed11981 <developerhuzaifa11981@gmail.com>
2024-08-16 03:00:27 +0600

    Rater ghum haram

--- email_templater/urls.py
+++ email_templater/urls.py
@@ -7,6 +7,7 @@ app_name = 'email_templater'
 urlpatterns = [
     path('email/create_email/', views.create_email, name='create_email'),
 
-    path('email/email_template/', views.email_template, name='email_template'),
+    path('email/email_template/<int:template_id>', views.email_template, name='email_template'),
+   
     
 ]
